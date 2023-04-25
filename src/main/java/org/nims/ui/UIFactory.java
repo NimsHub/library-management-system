29cfb809a3ebf5378ed70b71ee7ec31ff182e673
nirmala <nirmalasabaragamuwa@gmail.com>
2023-04-26 02:58:29 +0530

    refactoring

--- src/main/java/org/nims/ui/UIFactory.java
+++ src/main/java/org/nims/ui/UIFactory.java
@@ -1,15 +1,18 @@
 package org.nims.ui;
 
+import org.nims.commands.Commander;
 import org.nims.library.Library;
 
 public class UIFactory {
     private final Library library;
+    private final Commander commander;
 
-    public UIFactory(Library library) {
+    public UIFactory(Library library, Commander commander) {
         this.library = library;
+        this.commander = commander;
     }
-    public UIContract getUI(UI ui) {
-        if (ui == UI.CLI) return new CLI(library);
-        else return new GUI(library);
+    public UIContract getUI(UI ui, Commander commander) {
+        if (ui == UI.CLI) return new CLI(library, commander);
+        else return new GUI(library, commander);
     }
 }
