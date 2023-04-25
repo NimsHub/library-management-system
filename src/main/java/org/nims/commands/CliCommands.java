184ca893318b9b179b6642445429da5cf671a4a9
nirmala <nirmalasabaragamuwa@gmail.com>
2023-04-26 02:57:42 +0530

    implemented command pattern

--- src/main/java/org/nims/commands/CliCommands.java
+++ src/main/java/org/nims/commands/CliCommands.java
@@ -2,37 +2,39 @@ package org.nims.commands;
 
 
 import org.nims.library.Library;
-import org.nims.utils.Logger;
+import org.nims.utils.Printer;
 
 import java.util.Scanner;
 
 /**
  * This class holds all the commands for cli
  */
-public class CliCommands implements Commands{
+public class CliCommands implements Commands {
     private final Scanner scanner;
     private final Library library;
-    Logger logger = new Logger();
-    public CliCommands(Scanner scanner, Library library) {
+    private final Printer printer;
+
+    public CliCommands(Scanner scanner, Library library,Printer printer) {
         this.scanner = scanner;
         this.library = library;
+        this.printer = printer;
     }
 
 
     @Override
     public void addBook() {
         scanner.nextLine(); // to consume the newline character
-        System.out.print("Enter title: ");
+        printer.print("Enter title: ");
         String title = scanner.nextLine();
-        System.out.print("Enter author: ");
+        printer.print("Enter author: ");
         String author = scanner.nextLine();
-        library.addBook(title,author);
+        library.addBook(title, author);
     }
 
     @Override
     public void removeBook() {
         scanner.nextLine(); // to consume the newline character
-        System.out.print("Enter Id: ");
+        printer.print("Enter Id: ");
         String id = scanner.nextLine();
         library.removeBook(Integer.parseInt(id));
     }
@@ -40,36 +42,36 @@ public class CliCommands implements Commands{
     @Override
     public void borrowBook() {
         scanner.nextLine(); // to consume the newline character
-        System.out.print("Enter Id: ");
+        printer.print("Enter Id: ");
         String id = scanner.nextLine();
-        System.out.print("Enter Borrower's Name: ");
+        printer.print("Enter Borrower's Name: ");
         String borrower = scanner.nextLine();
-        library.borrowBook(Integer.parseInt(id),borrower);
+        library.borrowBook(Integer.parseInt(id), borrower);
     }
 
     @Override
     public void returnBook() {
         scanner.nextLine(); // to consume the newline character
-        System.out.print("Enter Id: ");
+        printer.print("Enter Id: ");
         String id = scanner.nextLine();
         library.returnBook(Integer.parseInt(id));
     }
 
     @Override
     public void availableBooks() {
-        logger.info(String.format("available books : %s",
+        printer.print(String.format("available books : %s",
                 library.availableBooks().toString()));
     }
 
     @Override
     public void borrowedBooks() {
-        logger.info(String.format("borrowed books : %s",
+        printer.print(String.format("borrowed books : %s",
                 library.borrowedBooks().toString()));
     }
 
     @Override
     public void overdueBooks() {
-        logger.info(String.format("overdue books : %s",
+        printer.print(String.format("overdue books : %s",
                 library.overdueBooks().toString()));
     }
 }
